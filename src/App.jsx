b4ed77acbab89b5fd072927417c55153e8744f7d
Malak Mahm0ud <malkmahmud942@gmail.com>
2026-06-30 04:50:01 +0200

    feat: implement Whiteboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Workspace from './pages/Workspace' 
 
 export default function App() {
   return (
-    <>
-      <HomePage/>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/workspace" element={<Workspace />} />
+    </Routes>
   )
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-createRoot(document.getElementById('root')).render(<App />) ;
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
